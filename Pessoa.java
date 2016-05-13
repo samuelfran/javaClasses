96242ace40578915c7094761783ccb9e3fbfc2f7
Julianosantosb <js.bentocontato@gmail.com>
2016-05-13 08:43:27 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,10 +1,20 @@
-package exercise;
+package exercises;
 
 public class Pessoa {
 	
 	private String nome;
 	private int idade;
 	private float altura;
+	
+	public Pessoa (String nome, int idade, float altura){
+		this.nome = nome;
+		this.idade = idade;
+		this.altura = altura;
+	}
+	
+	public Pessoa (){
+		
+	}
 	
 	public String getNome() {
 		return nome;
