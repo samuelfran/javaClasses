30c90b5cb73b5c4fe76f6ac5a845441cffaf6351
samuelfran <samuelparente3@hotmail.com>
2016-05-17 10:46:40 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,10 +1,9 @@
-package exercises;
+package EX;
 
 public class Pessoa {
-	
 	private String nome;
-	private int idade;
 	private float altura;
+	private int idade;
 	
 	public Pessoa (String nome, int idade, float altura){
 		this.nome = nome;
@@ -22,16 +21,18 @@ public class Pessoa {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public int getIdade() {
-		return idade;
-	}
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
 	public float getAltura() {
 		return altura;
 	}
 	public void setAltura(float altura) {
 		this.altura = altura;
 	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
 }
